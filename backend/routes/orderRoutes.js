ee33b7644025a2eb08bceb0dfd3f4ffe1731aafe
Ariukasq1 <Ariuka.sq@gmail.com>
2022-03-23 22:52:15 +0800

    update

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,11 @@
+import express from 'express';
+const orderRouter = express.Router();
+
+orderRouter.get(
+  '/mine',
+  isAuth,
+  expressAsyncHandler(async (req, res) => {
+    const orders = await Order.find({ user: req.user._id });
+    res.send(orders);
+  })
+);
